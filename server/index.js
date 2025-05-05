91b65f72f9555740c65631bec5ae7a234007464e
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-05 13:14:07 +0200

    Installed packages, dependencies and created a db connection to mongo

--- server/index.js
+++ server/index.js
@@ -11,6 +11,5 @@ app.use(express.json({extended: true}))
 app.use(cors({credentials: true, origin: ["http://localhost:5173"]}))
 app.use(upload())
 
-
-app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
+connect(process.env.MONGO_URL).then(app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))).catch(err=> console.log(err))
 
