39b0569b976ce523ac5e6527883a3a1bc14e5067
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-05 15:21:20 +0200

    Added and imported the routes handler

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const cors = require("cors")
 const upload = require("express-fileupload")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+const routes = require('./routes/routes')
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json({extended: true}))
 app.use(cors({credentials: true, origin: ["http://localhost:5173"]}))
 app.use(upload())
+
+app.use('/api', routes);
 
 app.use(notFound);
 app.use(errorHandler)
