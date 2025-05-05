878361610ab78e2032a6fcaf68a7003e3489b384
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-05 13:28:54 +0200

    imported notfound and errorhandler middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const {connect} = require("mongoose")
 require("dotenv").config()
 const cors = require("cors")
 const upload = require("express-fileupload")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware")
 
 const app = express()
 
@@ -10,6 +11,9 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json({extended: true}))
 app.use(cors({credentials: true, origin: ["http://localhost:5173"]}))
 app.use(upload())
+
+app.use(notFound);
+app.use(errorHandler)
 
 connect(process.env.MONGO_URL).then(app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))).catch(err=> console.log(err))
 
