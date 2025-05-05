45fd5e5a95fd22a565e61509108de06292c06a18
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-05 14:36:35 +0200

    Updated the user models

---
+++ server/models/userModels.js
@@ -0,0 +1,19 @@
+const {Schema, model} = require("mongoose")
+
+
+const userSchema = new Schema({
+  fullName: { type: stringify, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  profilePhoto: {
+    type: String,
+    default:"https://res.cloudinary.com/dyfskvja8/image/upload/v1746445145/avatar-default_zm90n6.svg",
+      },
+      bio:{type: String, default: "No bio yet"},
+      followers:[{type: Schema.Types.ObjectId, ref: "User"}],
+      following:[{type: Schema.Types.ObjectId, ref: "User"}],
+      bookmarks:[{type: Schema.Types.ObjectId, ref: "Post"}],
+      posts:[{type: Schema.Types.ObjectId, ref: "Post"}]
+},{timestamps: true});
+
+module.exports = model("User", userSchema)
