2396f6911beb4d0605dd97bf8ed06ed25347a8f5
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-06 12:08:52 +0200

    Added the authmiddleware to the routes handler

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,16 +1,17 @@
 const router = require("express").Router()
 
 const {registerUser, loginUser, getUser, getUsers, editUser, followUnfollowUser, changeUserAvatar} = require('../controllers/userControllers')
+const authMiddleware = require("../middleware/authMiddleware")
 
 
 //USER ROUTES
 router.post('/users/register', registerUser)
 router.post('/users/login', loginUser)
-router.get('users/:id', getUser)
+router.get('/users/:id', getUser)
 router.get('/users', getUsers)
-router.patch('/users/:id', editUser)
-router.get('/users/:id/follow-unfollow', followUnfollowUser)
-router.post('/users/avatar', changeUserAvatar)
+router.patch('/users/:id', authMiddleware, editUser)
+router.get('/users/:id/follow-unfollow', authMiddleware, followUnfollowUser)
+router.post('/users/avatar', authMiddleware, changeUserAvatar)
 
 
 module.exports = router;
