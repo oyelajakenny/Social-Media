39b69ffd1d6db1922bbb242d6b3e668916ef7314
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-06 12:11:13 +0200

    Updated the user models

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -2,7 +2,7 @@ const {Schema, model} = require("mongoose")
 
 
 const userSchema = new Schema({
-  fullName: { type: stringify, required: true },
+  fullName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   profilePhoto: {
