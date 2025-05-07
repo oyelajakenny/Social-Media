9b2eb2886c83162840c4465f893d5ac820dd756f
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-07 18:53:37 +0200

    Added authentication middleware to the post routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,25 +8,25 @@ const authMiddleware = require("../middleware/authMiddleware")
 //USER ROUTES
 router.post('/users/register', registerUser)
 router.post('/users/login', loginUser)
-router.get("/users/bookmarks", getUserBookmarks); // brought this route here to avoid conflict with get user
+router.get("/users/bookmarks", authMiddleware, getUserBookmarks); // brought this route here to avoid conflict with get user
 router.get('/users/:id', authMiddleware, getUser)
 router.get('/users', authMiddleware, getUsers)
 router.patch('/users/:id', authMiddleware, editUser)
 router.get('/users/:id/follow-unfollow', authMiddleware, followUnfollowUser)
 router.post('/users/avatar', authMiddleware, changeUserAvatar)
-router.get("/users/:id/posts",  getUserPosts)
+router.get("/users/:id/posts", authMiddleware, getUserPosts);
 
 
 
 //POST ROUTES
-router.post("/posts", createPost)
-router.get("/posts/:id", getPost)
-router.get("/posts", getPosts)
-router.patch("/posts/:id", updatePost)
-router.delete("/posts/:id", deletePost);
-router.get("/posts/:id/like", likeDislikePosts);
-router.get("/posts/following", getFollowingPosts);
-router.get("/posts/:id/bookmark", createBookmark);
+router.post("/posts", authMiddleware, createPost);
+router.get("/posts/following", authMiddleware, getFollowingPosts);
+router.get("/posts", authMiddleware, getPosts);
+router.get("/posts/:id", authMiddleware, getPost);
+router.patch("/posts/:id", authMiddleware, updatePost);
+router.delete("/posts/:id", authMiddleware, deletePost);
+router.get("/posts/:id/like", authMiddleware, likeDislikePosts);
+router.get("/posts/:id/bookmark", authMiddleware, createBookmark);
 
 
 
