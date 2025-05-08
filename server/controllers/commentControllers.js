285f52a119b82d10688c56c54cf558fd6996e03a
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-08 23:06:59 +0200

    Implemented the create comment controller

---
+++ server/controllers/commentControllers.js
@@ -0,0 +1,52 @@
+const HttpError = require("../models/errorModels")
+const commentModel = require("../models/commentsModels")
+const postModel = require("../models/postModels")
+const UserModel = require("../models/userModels")
+
+
+//.............................CREATE COMMENT
+//POST : api/comments/:Id
+//PROTECTED
+
+const createComment =async (req, res, next)=>{
+    try {
+        const {postId} = req.params;
+        const {comment} = req.body;
+        if(!comment){
+            return next(new HttpError("Please write a comment"), 422)
+        }
+        //get comment creator from db
+        const commentCreator = await UserModel.findById(req.user.id)
+        const newComment = await commentModel.create({creator: {creatorId: req.user.id, creatorName: commentCreator?.fullName, creatorPhoto:commentCreator?.profilePhoto}, comment, postId})
+        await postModel.findByIdAndUpdate(postId, {$push: {comments: newComment?._id}}, {new: true})
+        res.json(newComment)
+    } catch (error) {
+        return next(new HttpError)
+    }
+}
+
+//.............................GET POST COMMENT
+//POST : api/comments/:postId
+//PROTECTED
+
+const getPostComments =async (req, res, next)=>{
+    try {
+        res.json("get post comments")
+    } catch (error) {
+        return next(new HttpError)
+    }
+}
+
+//.............................DELETE COMMENT
+//POST : api/comments/:commentId
+//PROTECTED
+
+const deleteComments =async (req, res, next)=>{
+    try {
+        res.json("Delete comments")
+    } catch (error) {
+        return next(new HttpError)
+    }
+}
+
+module.exports={createComment, getPostComments, deleteComments}
