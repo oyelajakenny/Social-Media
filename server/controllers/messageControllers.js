3341d105219f758d1adf994174ed2235c4a410e7
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-09 11:33:47 +0200

    Implemented the create message controller

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -9,7 +9,18 @@ const messageModel = require("../models/messageModel")
 
 const createMessage = async(req, res, next)=>{
     try {
-        res.json("Create Message")
+        const {receiverId} = req.params;
+        const {messageBody}= req.body;
+        //check if there's already a conversation between current user and receiver
+        let conversation = await conversationModel.findOne({participants: {$all:[req.user.id, receiverId]}})
+        //create a new conversation if none was found
+        if(!conversation){
+            conversation = await conversationModel.create({participants: [req.user.id, receiverId], lastMessage:{text: messageBody, senderId: req.user.id}})
+        }
+        //create a new message
+        const newMessage = await messageModel.create({conversationId: conversation._id, senderId: req.user.id, text: messageBody})
+        await conversation.updateOne({lastMessage: {text: messageBody, senderId: req.user.id}})
+        res.json(newMessage)
     } catch (error) {
         return next(new HttpError(error))
     }
