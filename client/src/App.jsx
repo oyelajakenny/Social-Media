c37b69d14ed03db2d39464828ed1c414804227f2
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-15 14:17:26 +0200

    Updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,36 @@
 import React from 'react'
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import RootLayout from './RootLayout'
 import ErrorPage from './pages/ErrorPage';
 import MessagesList from "./components/MessagesList";
 import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
 import SinglePost from "./pages/SinglePost"
+import Home from "./pages/Home"
+import Bookmarks from "./pages/Bookmarks"
 import Login from "./pages/Login"
 import Logout from "./pages/Logout"
 import Register from "./pages/Register"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "messages", element: <MessagesList /> },
-      { path: "messages/receiverId", element: <Messages /> },
-      { path: "bookmarks", element: <Bookmarks /> },
-      { path: "users/:id", element: <Profile /> },
-      { path: "posts/:id", element: <SinglePost /> },
-    ],
+  {path: "/", element: <RootLayout/>, errorElement: <ErrorPage />, children: [
+      {index: true, element: <Home />},
+      {path: "messages", element: <MessagesList />},
+      {path: "messages/:receiverId", element: <Messages />},
+      {path: "bookmarks", element: <Bookmarks />},
+      {path: "users/:id", element: <Profile />},
+      {path: "posts/:id", element: <SinglePost />},
+    ]
   },
-  { path: "login", element: <Login /> },
-  { path: "register", element: <Register /> },
-  { path: "login", element: <Logout /> },
+  {path: "/login", element: <Login />},
+  {path: "/register", element: <Register />},
+  {path: "/logout", element: <Logout />},
 ]);
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
