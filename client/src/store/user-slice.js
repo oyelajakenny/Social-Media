c8c343e975357487b5524d58145267ed51847576
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-15 14:44:53 +0200

    Moved the user reducers to the correct file

---
+++ client/src/store/user-slice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = create({
+  name: "user",
+  initialState: {
+    currentUser: JSON.parse(localStorage.getItem("currentuser")) || null,
+    socket: null,
+    onlineUsers: [],
+  },
+  reducers: {
+    changeCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    setSocket: (state, action) => {
+      state.socket = action.payload;
+    },
+    setOnlineUsers: (state, action) => {
+      state.onlineUsers = action.payload;
+    },
+  },
+});
+
+export const userActions = userSlice.actions;
+export default userSlice;
