c509935641fffa40ed78e98fcfdc6dc4801dcefc
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-15 14:18:35 +0200

    Fixed issue with app not rendering well

--- client/src/RootLayout.jsx
+++ client/src/RootLayout.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
+import Widgets from "./components/Widgets";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <>
+      <Navbar />
+      <main className="main">
+        <div className="container main_container ">
+          <SideBar />
+          <Outlet/>
+          <Widgets />
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
