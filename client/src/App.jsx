d4adcb116e1a7728ca348a24da8bedbb1f867df7
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-15 12:51:50 +0200

    Added additional routes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import MessagesList from "./components/MessagesList";
 import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
 import SinglePost from "./pages/SinglePost"
+import Login from "./pages/Login"
+import Logout from "./pages/Logout"
+import Register from "./pages/Register"
 
 const router = createBrowserRouter([
   {
@@ -16,12 +19,14 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "messages", element: <MessagesList /> },
       { path: "messages/receiverId", element: <Messages /> },
-      { path: "bookmarks", element: <Bookmarks/> },
+      { path: "bookmarks", element: <Bookmarks /> },
       { path: "users/:id", element: <Profile /> },
       { path: "posts/:id", element: <SinglePost /> },
-      
     ],
   },
+  { path: "login", element: <Login /> },
+  { path: "register", element: <Register /> },
+  { path: "login", element: <Logout /> },
 ]);
 
 
