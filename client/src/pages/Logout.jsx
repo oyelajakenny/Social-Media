9a382050339a697076d4328c2d02d233f5e8ecc7
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-16 18:44:47 +0200

    Implemented the logout functionality

--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom"
+import {useDispatch} from "react-redux"
+import {useEffect} from "react"
+import {userActions} from "../store/user-slice"
 
 const Logout = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(userActions.changeCurrentUser({}))
+    localStorage.setItem("currentUser", null);
+    navigate("/login")
+  },[])
   return (
     <div>Logout</div>
   )
