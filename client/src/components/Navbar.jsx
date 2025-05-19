3ba4ca8dd4e1fc3c270782578202f50a0544bdee
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-19 23:36:29 +0200

    Added some styling to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
   }, [])
   return (
     <div className="w-screen">
-      <div className=" px-8 flex justify-between items-center p-4 bg-white shadow-md ">
+      <div className=" px-8 flex justify-between items-center p-2 bg-white shadow-md ">
         <Link className="font-bold text-2xl" to="/">
           Tiri Social
         </Link>
@@ -41,8 +41,8 @@ const Navbar = () => {
             type="text"
             placeholder="Search"
           />
-          <button type="submit">
-            <CiSearch className="absolute right-3 top-3" size={20} />
+          <button type="submit" className='absolute right-0 top-0 bg-blue-400 w-11 h-full rounded-r-lg p-2 text-white hover:bg-blue-500'> 
+            <CiSearch className="absolute right-3 top-3" size={20}  />
           </button>
         </form>
         <div className="flex items-center gap-7">
