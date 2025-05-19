a8a77f5371d8580f3629a4d6b369aca90417316c
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-19 23:38:38 +0200

    Added some styling to the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import CreatePost from '../components/CreatePost'
+import axios from 'axios'
+
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+  const token = useSelector((state)=>state?.user?.currentUser?.token)
+
+  const createPost = async(data)=>{
+    setError("")
+    try {const response = await axios.post(`${import.meta.env.VITE_API_URL}/posts`, data, {
+    withCredentials: true, headers:{Authorization: `Bearer ${token}`}})
+    const newPost = response?.data;
+    setPosts([newPost, ...posts])
+      
+    } catch (error) {
+      setError(error?.response?.data?.message)
+    }
+  }
   return (
-    <div>Home</div>
+    <div>
+      <CreatePost onCreatePost={createPost} error={error}/>
+    </div>
   )
 }
 
