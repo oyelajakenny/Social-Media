8a531180581e3eb127df80b2c7b2534acf61e1f4
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-20 17:15:50 +0200

    Added styling to the Feeds component

--- client/src/components/Feeds.jsx
+++ client/src/components/Feeds.jsx
@@ -3,7 +3,7 @@ import Feed from './Feed'
 
 const Feeds = ({posts}) => {
   return (
-    <div>
+    <div className='flex flex-col gap-4 mt-10'>
         {posts.length < 1 ? <p>No posts found</p>: posts?.map(post=><Feed key={post?._id} post={post}/>)}
     </div>
   )
