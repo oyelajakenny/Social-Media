d77b94f453790e5ca42122ccd91ccb697d4e9b42
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-25 20:03:33 +0200

    Created the Like and dislike post component

---
+++ client/src/components/LIkeDislikePost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LIkeDislikePost = () => {
+  return (
+    <div>LIkeDislikePost</div>
+  )
+}
+
+export default LIkeDislikePost
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -6,10 +6,7 @@ const userSchema = new Schema(
     fullName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    profilePhoto: {
-      type: String,
-      default:
-        "https://res.cloudinary.com/dyfskvja8/image/upload/v1746445145/avatar-default_zm90n6.svg",
+    profilePhoto: { type: String, default: "https://res.cloudinary.com/dyfskvja8/image/upload/v1746445145/avatar-default_zm90n6.svg",
     },
     bio: { type: String, default: "No bio yet" },
     followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
