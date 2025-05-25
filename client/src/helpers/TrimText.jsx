896b3549ca0d0b22264f751831d99f78c715d876
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-25 20:22:23 +0200

    Created the Trim component to trim text

---
+++ client/src/helpers/TrimText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrimText = ({item, maxLength}) => {
+  return (
+    <>
+    {item?.length > maxLength ? item?.substring(0, maxLength)+ "..." : item}
+    </>
+  )
+}
+
+export default TrimText
