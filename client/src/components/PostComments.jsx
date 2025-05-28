561f69e0f0286049fe2d5780f23d55e92ad8b36d
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-28 19:28:15 +0200

    Added a default value to the comment textarea

--- client/src/components/PostComments.jsx
+++ client/src/components/PostComments.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { FaRegTrashAlt } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import TimeAgo from 'react-timeago';
 
-const PostComments = () => {
+const PostComments = ({comment, onDeleteComment}) => {
+    const token = useSelector(state => state?.user?.currentUser?.token);
+    const userId = useSelector(state => state?.user?.currentUser?.id);
+
+    const deleteComment = async () => {
+        onDeleteComment(comment?._id);
+    }
   return (
-    <div>PostComments</div>
+    <li>
+        <div>
+            <div>
+                <img src={comment?.creator?.creatorPhtoto} alt="profile" className="w-10 h-10 rounded-full" />
+            </div>
+            <div>
+                <div>
+                    <h5>{comment?.creator?.creatorname}</h5>
+                    <small><TimeAgo date={comment?.createdAt}/></small>
+                </div>
+                <p>{comment?.comment}</p>
+            </div>
+        </div>
+        {userId === comment?.creator?.creatorId && (
+            <button onClick={deleteComment} className="text-red-500 hover:underline">
+                <FaRegTrashAlt className="inline-block mr-1" />
+            </button>
+        )}  
+    </li>
   )
 }
 
