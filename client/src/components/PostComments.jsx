e34a44d4d3f0dd6b84505484d42d2b99285971c9
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-28 23:18:39 +0200

    Updated the comment box styling

--- client/src/components/PostComments.jsx
+++ client/src/components/PostComments.jsx
@@ -11,14 +11,14 @@ const PostComments = ({comment, onDeleteComment}) => {
         onDeleteComment(comment?._id);
     }
   return (
-    <li>
-        <div>
+    <li className="flex items-start justify-between gap-4 py-2 px-3 border-b border-gray-200 bg-white rounded-lg shadow-sm my-2">
+        <div className="flex items-start gap-4">
             <div>
                 <img src={comment?.creator?.creatorPhtoto} alt="profile" className="w-10 h-10 rounded-full" />
             </div>
             <div>
-                <div>
-                    <h5>{comment?.creator?.creatorname}</h5>
+                <div className="flex items-center gap-2 mb-1">
+                    <h5 className='text-lg font-semibold'>{comment?.creator?.creatorName}</h5>
                     <small><TimeAgo date={comment?.createdAt}/></small>
                 </div>
                 <p>{comment?.comment}</p>
