5e8da51d643e67172aaa9b20f8510400a4c11cb3
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-29 16:46:59 +0200

    Changed the comment field to a self closing textarea

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -108,9 +108,8 @@ useEffect(() => {
             placeholder="Add a comment..."
             className="border p-2 rounded-lg w-full bg-slate-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
             onChange={(e) => setComment(e.target.value)}
-          >
-            {comment}
-          </textarea>
+          />
+           
           <button
             type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded-lg"
