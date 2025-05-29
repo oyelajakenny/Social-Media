03ab9faa31733b1bad63702766db6321b6fc1c83
Oyelaja Hussein <premiumitng@gmail.com>
2025-05-29 16:16:06 +0200

    Made the API call to fetch bookmarked posts from the backend

--- client/src/pages/Bookmarks.jsx
+++ client/src/pages/Bookmarks.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { use } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import Feed from "../components/Feed";
 
 const Bookmarks = () => {
-  return (
-    <div>Bookmarks</div>
-  )
-}
+  const [bookmarks, setBookmarks] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const token = useSelector((state) => state?.user?.currentUser?.token);
 
-export default Bookmarks
+  //GET Bookmarks of Logined User
+
+  const getBookmarks = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/boookmarks`,
+        {
+          withCredentials: true,
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setBookmarks(response?.data.bookmarks);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+  React.useEffect(() => {
+    getBookmarks();
+  }, []);
+
+  return (
+    <section>
+      {bookmarks?.length < 1 ? (
+        <p>No posts bookmarked</p>
+      ) : (
+        bookmarks?.map((bookmark) => (
+          <Feed key={bookmark?._id} post={bookmark} />
+        ))
+      )}
+    </section>
+  );
+};
+
+export default Bookmarks;
