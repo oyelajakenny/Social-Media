a29207a896b074a1339699917ca8525b15c80509
Oyelaja Hussein <premiumitng@gmail.com>
2025-06-02 22:54:30 +0200

    Added an header info to the bookmark page

---
+++ client/src/components/HeaderInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderInfo = ({text}) => {
+  return (
+    <div className='text-center py-5  rounded-lg bg-white shadow-md font-bold'>{text}</div>
+  )
+}
+
+export default HeaderInfo
