7186b8e6f3cfca81d9c4f5e266f2e2ebdb1d9278
Oyelaja Hussein <premiumitng@gmail.com>
2025-06-03 11:20:08 +0200

    Made an API call to fetch user profile from the backend

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
 
 const UserProfile = () => {
+    const token = useSelector((state) => state.user?.currentUser?.token);
+    const loggedInUser = useSelector(state => state?.user?.currentUser?.id);
+
+
+    const [user, setUser] = React.useState({})
+    const [followsuser, setFollowsUser] = useState(user?.followers?.includes(loggedInUser))
+    const [avatar, setAvatar] = useState(user?.profilePhoto)
+    const {id:userId} = useParams()
+    
+
+
+    const getUser = async ()=>{
+        try {
+            const response = await axios.get(`${import.meta.ENV.VITE_API_URL}/users/${userId}`, {withCredentials: true, headers:{Authorization: `Bearer ${token}`}})
+            setUser(response?.data)
+            setFollowsUser(response?.data?.followers?.includes(loggedInUser))
+            setAvatar(response?.data?.profilePhoto)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(()=>{
+        getUser()
+    },[])
+
+    console.log(user)
   return (
-    <div>UserProfile</div>
+    <section>
+<div>
+    
+</div>
+    </section>
   )
 }
 
