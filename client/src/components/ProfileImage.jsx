eb1c0038db69220af2807ef1ae5566ec846c8441
Oyelaja Hussein <premiumitng@gmail.com>
2025-06-04 16:18:23 +0200

    Added a condition to display a default image if the user has not uploaded a profile image

--- client/src/components/ProfileImage.jsx
+++ client/src/components/ProfileImage.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const ProfileImage = ({image}) => {
+const ProfileImage = ({ image }) => {
+  const [imgError, setImgError] = useState(false);
+  const defaultImage =
+    "https://res.cloudinary.com/dyfskvja8/image/upload/v1746445145/avatar-default_zm90n6.svg";
+
+  const handleImageError = () => {
+    setImgError(true);
+  };
+
   return (
     <div>
-        <img src={image} alt="profile" className="w-10 h-10 rounded-full"  /> 
-
+      <img
+        src={imgError || !image ? defaultImage : image}
+        alt="profile"
+        className="w-10 h-10 rounded-full object-cover"
+        onError={handleImageError}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ProfileImage
+export default ProfileImage;
