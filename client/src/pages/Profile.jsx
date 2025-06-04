ccd7b7fbcf57947f9d9d7414d5bdef9d0ae588d9
Oyelaja Hussein <premiumitng@gmail.com>
2025-06-04 16:23:11 +0200

    Added a styling to the user profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,9 +3,10 @@ import UserProfile from '../components/UserProfile'
 
 const Profile = () => {
   return (
-<section>
-  <UserProfile/>
-</section>  )
+    <section className="p-4 max-w-[700px] min-w-[700px] bg-white rounded-lg shadow-md">
+      <UserProfile />
+    </section>
+  );
 }
 
 export default Profile
